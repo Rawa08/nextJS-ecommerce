ca2fdd01e6f531f9eeddb69d4da88def500fd565
Rawa Aref <rawa_aref@hotmail.com>
2022-01-05 19:58:48 +0100

    db config typo

--- utils/db.js
+++ utils/db.js
@@ -13,17 +13,13 @@ const connectDb = async () => {
         connection.isConnected = moongose.connections[0].readyState;
 
         if(connection.isConnected === 1) {
-            console.log("Alredy connected, use previeus connection");
+            console.log("Already connected, use previous connection");
             return;
         }
         await moongose.disconnect();
     }    
 
-    const db = await moongose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    });
+    const db = await moongose.connect(process.env.DB_URI);
 
     console.log("Db connected");
 
@@ -35,33 +31,11 @@ const disconnectDb = async () => {
     if(connection.isConnected) {
         if(process.env.NODE_ENV === 'production'){
             await moongose.disconnect();
-            connect.isConnected = false;
+            connection.isConnected = false;
         }else {
-            console.log("Db not disconnected - Dev ENV")
+            console.log("Db not disconnected - NODE ENV = DEV")
         }
     }
-
-    if(moongose.connections.length > 0){
-        connection.isConnected = moongose.connections[0].readyState;
-
-        if(connection.isConnected === 1) {
-            console.log("Alredy connected, use previeus connection");
-            return;
-        }
-        await moongose.disconnect();
-    }    
-
-    const db = await moongose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    });
-
-    console.log("Db connected");
-
-    connection.isConnected = db.connections[0].readyState;
-
-
 };
 
 const db = {connectDb, disconnectDb};
