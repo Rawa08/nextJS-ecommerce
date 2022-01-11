03b02357eba56b9b4adbc9d760038cb9e5e278c6
Rawa Aref <rawa_aref@hotmail.com>
2022-01-11 20:17:27 +0100

    Order Model

---
+++ models/Order.js
@@ -0,0 +1,28 @@
+import moongose from 'mongoose';
+
+const orderSchema = new moongose.Schema({
+   
+  user:{type: moongose.Schema.Types.ObjectId, ref:'User', required:true},
+  orderItems: [{title: {type: String, required:true}, description:{type: String, required:true}, image:{type: String, required:true}, 
+    quantity: {type: Number, required:true}, price:{type: Number, required:true} ,rating: {
+    rate: {type: Number},
+    count: {type: Number}
+  } }],
+  shippingAddress: {fullName:{type: String, required:true}, address: {type: String, required:true}, city: {type: String, required:true}
+  , postalCode: {type: String, required:true}, country: {type: String, required:true}},
+  paymentMethod: {type: String, required:true},
+  itemsPrice: {type: Number, required:true},
+  vatAmount: {type: Number, required:true},
+  shippingPrice: {type: Number, required:true},
+  totalPrice: {type: Number, required:true},
+  isPaid: {type: Boolean, required:true, default:false},
+  isDeliverd: {type: Boolean, required:true, default:false},
+  paidAt: {type: Date},
+  deliverdAt: {type: Date},
+},{
+    timestamps:true
+});
+
+const Order = moongose.models.Product || moongose.model('Order', orderSchema);
+
+export default Product;
